e92b451d1cc9192dfdcd68d28b182e16d8dbc79e
PlaviAjvar <zaslanjezadataka@gmail.com>
2020-07-09 15:26:31 +0200

    random commmit 2

--- my_zncc.m
+++ my_zncc.m
@@ -2,14 +2,5 @@ function m = my_zncc(w1, w2)
 	w1 = w1 - mean(w1(:));
 	w2 = w2 - mean(w2(:));
 	denom = sqrt( sum(sum(w1.^2))*sum(sum(w2.^2)) );
-
-	if denom < 1e-10
-        if sum >= 0
-            m = Inf;
-        else
-            m = -Inf;
-        end
-	else
-		m = sum(sum((w1.*w2))) / denom;
-    end
+	m = sum(sum((w1.*w2))) / denom;
 end
